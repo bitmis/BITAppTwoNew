a29eb2c659955656681a50ca130714d88d7a3f6f
bitmis <pmg@ucsc.cmb.ac.lk>
2022-08-01 16:32:22 +0530

    applicantb no generation

--- src/app/reg-selection/reg-selection.page.ts
+++ src/app/reg-selection/reg-selection.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { map } from 'rxjs/operators';
- 
+import { RegSelectionService } from '../services/reg-selection.service';
+
 
 @Component({
   selector: 'app-reg-selection',
@@ -10,28 +11,68 @@ import { map } from 'rxjs/operators';
 })
 export class RegSelectionPage implements OnInit {
 
-  login_response: any;
-  id_no:string;
-  eligible_year:string;
-  prev_bit_regno:string;
+  status_response: any;
+  id_no: string;
+  eligible_year: string;
+  prev_bit_regno: string;
   activatedRoute: any;
 
-  constructor(  private route: ActivatedRoute,private router: Router) { }
+
+  show_start_application: boolean = false;
+  show_continue_application: boolean = false;
+  show_payment_application: boolean = false;
+
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private regSelectionService: RegSelectionService) { }
 
   ngOnInit() {
-   
-    this.id_no= this.route.snapshot.paramMap.get('id_no');
-    this.prev_bit_regno= this.route.snapshot.paramMap.get('registration_no');
-    this.eligible_year= this.route.snapshot.paramMap.get('year');
+
+    this.id_no = this.route.snapshot.paramMap.get('id_no');
+    this.prev_bit_regno = this.route.snapshot.paramMap.get('registration_no');
+    this.eligible_year = this.route.snapshot.paramMap.get('year');
 
 
     console.log("id_no " + this.id_no);
     console.log("prev_bit_regno " + this.prev_bit_regno);
     console.log("eligible_year " + this.eligible_year);
-   
- 
 
-     
+
+    this.getApplicationStatus();
+
+  }
+
+  getApplicationStatus() {
+
+    this.regSelectionService.getApplicationStatus(this.prev_bit_regno).subscribe((res) => {
+
+      this.status_response = res;
+      console.log("- -- " + this.status_response);
+      if (this.status_response == null) {
+
+        this.show_start_application = true;
+      } else {
+
+
+        
+       }
+
+    });
+
+  }
+
+  createApplicationNumber(){
+
+    console.log("application no created");
+    this.regSelectionService.generateApplicationNo(this.prev_bit_regno , this.eligible_year).subscribe((res) => {
+
+      this.status_response = res;
+      console.log("- -- " + this.status_response);
+    });
+
+
+
   }
 
 }
--- src/app/services/reg-selection.service.ts
+++ src/app/services/reg-selection.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { LateralApplicantInfo } from '../interface/lateral-applicant-info';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 
@@ -10,7 +9,7 @@ import { retry, catchError } from 'rxjs/operators';
 export class RegSelectionService {
 
   // Define API
-  apiURL = 'http://localhost:8080';
+  apiURL = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient) { }
   // Http Options
@@ -26,19 +25,30 @@ export class RegSelectionService {
       .pipe(retry(1), catchError(this.handleError));
   }
 
-    // Error handling
-    handleError(error: any) {
-      let errorMessage = '';
-      if (error.error instanceof ErrorEvent) {
-        // Get client-side error
-        errorMessage = error.error.message;
-      } else {
-        // Get server-side error
-        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-      }
-      window.alert(errorMessage);
-      return throwError(() => {
-        return errorMessage;
-      });
+
+  // HttpClient API get() method => Fetch application status
+  generateApplicationNo(prev_registration_no: string  , year: string): Observable<any> {
+    return this.http.get<any>(this.apiURL + '/save_lateral_application_number/' + prev_registration_no +"/"+ year)
+      .pipe(retry(1), catchError(this.handleError));
+  }
+
+
+  // Error handling
+  handleError(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
+    window.alert(errorMessage);
+    return throwError(() => {
+      return errorMessage;
+    });
+  }
+
+
+
 }
